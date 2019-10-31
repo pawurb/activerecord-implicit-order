b1bf690cd73c20a0a02ea9c875a016e1b1a3a1c8
pawurb <p.urbanek89@gmail.com>
2019-10-31 23:24:32 +0100

    0.6.0

--- lib/activerecord-uuid-order.rb
+++ lib/activerecord-uuid-order.rb
@@ -2,13 +2,15 @@ require 'activerecord-uuid-order/version'
 require 'activerecord-uuid-order/core_extensions'
 
 module ActiveRecordUUIDOrder
+  def self.included(base)
+    base.class_variable_set(:@@implicit_order_column, nil)
+  end
+
   def implicit_order_column=(value)
-    class_variable_set(:@@implicit_order_column , value)
+    class_variable_set(:@@implicit_order_column, value)
   end
 
   def implicit_order_column
     class_variable_get(:@@implicit_order_column)
-  rescue NameError
-    nil
   end
 end
