2784c797f534534262548498a357f5b8235fe62e
pawurb <p.urbanek89@gmail.com>
2019-10-31 23:05:41 +0100

    0.4.0

--- lib/activerecord-uuid-order.rb
+++ lib/activerecord-uuid-order.rb
@@ -2,6 +2,8 @@ require 'activerecord-uuid-order/version'
 require 'activerecord-uuid-order/core_extensions'
 
 module ActiveRecordUUIDOrder
+  @@implicit_order_column = nil
+
   def implicit_order_column=(value)
     @@implicit_order_column = value
   end
