2111bea53f98a99f992b5efd0467f3716d220dc4
pawurb <p.urbanek89@gmail.com>
2019-11-02 19:29:44 +0100

    0.8.0

--- lib/activerecord-uuid-order/core_extensions.rb
+++ lib/activerecord-uuid-order/core_extensions.rb
@@ -2,7 +2,11 @@ module ActiveRecord
   module FinderMethods
     def ordered_relation
       if order_values.empty? && (implicit_order_column || primary_key)
-        order(arel_attribute(implicit_order_column || primary_key).asc)
+        if implicit_order_column.present? && implicit_order_column != primary_key
+          order(arel_attribute(implicit_order_column).asc, arel_attribute(primary_key).asc)
+        else
+          order(arel_attribute(implicit_order_column || primary_key).asc)
+        end
       else
         self
       end
