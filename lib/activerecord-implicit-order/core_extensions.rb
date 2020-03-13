1dd34641994bc05a0720f907fbf595d91c81564f
Max Schwenk <maschwenk@gmail.com>
2020-03-13 18:24:55 -0400

    Gracefully fallback when implicit_order_column not defined (#1)
    * Gracefully fallback to old behavior when implicit_order_column not defined
    
    * Slight adjustment

--- lib/activerecord-implicit-order/core_extensions.rb
+++ lib/activerecord-implicit-order/core_extensions.rb
@@ -1,11 +1,13 @@
 module ActiveRecord
   module FinderMethods
     def ordered_relation
-      if order_values.empty? && (implicit_order_column || primary_key)
-        if implicit_order_column && primary_key && implicit_order_column != primary_key
+      implicit_order_column_defined = respond_to?(:implicit_order_column) && implicit_order_column
+
+      if order_values.empty? && (implicit_order_column_defined || primary_key)
+        if implicit_order_column_defined && primary_key && implicit_order_column != primary_key
           order(arel_attribute(implicit_order_column).asc, arel_attribute(primary_key).asc)
         else
-          order(arel_attribute(implicit_order_column || primary_key).asc)
+          order(arel_attribute(implicit_order_column_defined || primary_key).asc)
         end
       else
         self
